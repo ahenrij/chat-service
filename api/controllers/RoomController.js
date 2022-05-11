82da20a3ca2e8b028c01b4691a3dda1ddefded54
Henri Aïdasso <ahenrij@gmail.com>
2022-05-11 23:08:08 +0200

    get room history messages

--- api/controllers/RoomController.js
+++ api/controllers/RoomController.js
@@ -7,6 +7,24 @@
 
 module.exports = {
   
+    find: function(req, res) {
+        Room.find().populate('members').exec((error, data) => {
+            if (error) { return res.serverError(error); }
+            if (!data) { return res.notFound({ message: 'Aucune donnée correspondante' }); }
+            res.ok({ data: data });
+        });
+    },
 
+    history: function(req, res) {
+        let id = req.param('id');
+        if (!id) {
+            res.badRequest({ message: 'Please make sure request parameters are correct.' })
+        }
+        Room.find({ id }).populate('messages').exec((error, data) => {
+            if (error) { return res.serverError(error); }
+            if (!data) { return res.serverError({ message: 'Aucune donnée correspondante' }); }
+            res.ok({ data: data.messages });
+        });
+    }
 };
 

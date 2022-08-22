16d2773f34d61078ce3b05d19dbb21487dd2c2d5
Henri Aïdasso <ahenrij@gmail.com>
2022-08-22 13:54:25 +0200

    check if room exists

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -10,12 +10,18 @@ const checkSocket = (process.env.STRICTLY_CHECK_SOCKET === 'true');
 
 module.exports = {
 
-  send: function (req, res) {
+  send: async function (req, res) {
     if (checkSocket && !req.isSocket) {
       return res.badRequest({ message: 'This must be a socket request.' });
     }
-    let roomId = req.body.room;
-    Message.create(req.body).fetch().exec(async (error, data) => { // data is createdMessage
+    const roomId = req.body.room;
+    // check if room exists
+    var room = await Room.findOne({ id: roomId });
+    if (!room) {
+      return res.badRequest({ message: 'Room not found.' });
+    }
+    // create message 
+    Message.create(req.body).fetch().exec(async (error, data) => {
       if (error) { return res.serverError(error); }
       // get chat history
       const history = await Message.find({ room: roomId });
